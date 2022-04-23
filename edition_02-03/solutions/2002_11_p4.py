73361ab1feab1e10500d2ab6931dcbdbd9a70101
Vineeth <ncvineeth97@gmail.com>
2022-04-22 22:14:47 -0400

    Finished November 2002 codes

---
+++ edition_02-03/solutions/2002_11_p4.py
@@ -0,0 +1,35 @@
+# November 2002, Problem 4: MONEY PRIZE
+# https://github.com/v1neethnc/dwite-contest-solutions
+
+with open("../inputs/2002_11_problem4.txt") as file_data:
+
+	# Read the data and create the grid
+	data = file_data.read().split("\n")
+	grid = [[int(j) for j in i.split(' ')] for i in data]
+
+	# The final grid consists of the top 5 costs to reach each element
+	# Calculate the vertical and horizontal costs from the start element
+	final = [[[sum(grid[len(grid)-1][:i])] for i in range(1, len(grid[0])+1)]]
+	prev = final[0][0][0]
+	for i in range(len(grid[0])-2, -1, -1):
+		final = [[[prev + grid[i][0]]]] + final
+		prev = prev + grid[i][0]
+
+	# Calculate the costs at each step
+	for row in range(len(grid)-2, -1, -1):
+		for col in range(1, len(grid[0])):
+			new_val = []
+
+			# Add the cell value to the path costs and update the array of costs
+			for i in [final[row+1][col], final[row][col-1]]:
+				tmp = [x + grid[row][col] for x in i]
+				new_val.extend(tmp)
+			new_val.sort()
+			
+			# Save the top five costs
+			final[row].append(new_val[-5:])
+	
+	# Sort and print the cost list at the destination 
+	final[0][7].sort()
+	for i in final[0][7][::-1]:
+		print(i)
---
+++ edition_02-03/solutions/2002_11_p5.py
@@ -0,0 +1,20 @@
+# November 2002, Problem 5: NOW I KNOW MY ABC'S
+# https://github.com/v1neethnc/dwite-contest-solutions
+
+with open("2002_11_problem5.txt") as file_data:
+
+	# Read the data from the lines
+	data = file_data.read().split("\n")
+	for i in data:
+
+		# Normalize the sentence by converting everything to uppercase
+		line = i.upper()
+
+		# Create a hashmap of letter and corresponding counts
+		dict_map = {chr(i): line.count(chr(i)) for i in range(ord('A'), ord('Z') + 1) if line.count(chr(i)) > 0}
+		
+		# Create the string in the specified format and print the result
+		res = ''
+		for i in dict_map.keys():
+			res += i + '-' + str(dict_map[i]) + ':'
+		print(res[:-1])
