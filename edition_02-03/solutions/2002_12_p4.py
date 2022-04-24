245214f5836323612d10f433fa069769da26c455
Vineeth <ncvineeth97@gmail.com>
2022-04-24 19:14:24 -0400

    Dec 2002 solution 4

---
+++ edition_02-03/solutions/2002_12_p4.py
@@ -0,0 +1,34 @@
+# December 2002, Problem 4: AREA OF A TRIANGLE
+# https://github.com/v1neethnc/dwite-contest-solutions
+
+
+from math import sqrt
+
+# Function to calculate the triangle sides given the points
+def sides_calc(p1, p2, p3):
+	return round(sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2), 3), \
+	 	   round(sqrt((p3[0] - p1[0])**2 + (p3[1] - p1[1])**2), 3), \
+		   round(sqrt((p2[0] - p3[0])**2 + (p2[1] - p3[1])**2), 3)
+
+
+with open("2002_12_problem4.txt") as file_data:
+
+	# Create list of lists, with each sublist corresponding to a case
+	data = [[int(j) for j in i.split(' ')] for i in file_data.read().split("\n")]
+
+	# Iterate through the cases
+	for line in data:
+
+		# Get the points and calculate the sides
+		p1, p2, p3 = line[0:2], line[2:4], line[4:]
+		d1, d2, d3 = sides_calc(p1, p2, p3)
+		
+		# Check for triangle inequality violations
+		if d1 + d2 <= d3 or d2 + d3 <= d1 or d3 + d1 <= d2:
+			print("NOT POSSIBLE")
+			
+		else:
+			# Calculate area using Heron's formula
+			s = (d1 + d2 + d3) / 2
+			area = round(sqrt(s * (s-d1) * (s-d2) * (s-d3)), 1)
+			print(f"{area:.2f}")
